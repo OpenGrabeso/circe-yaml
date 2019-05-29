1d688e50e4ec1cc301d6da46cf256694c54743be
Ondřej Španěl <OndrejSpanel@users.noreply.github.com>
2019-05-29 15:22:25 +0200

    Basic "double" tests failing (int ones pass).

---
+++ src/test/scala/io/circe/yaml/NumberTests.scala
@@ -0,0 +1,31 @@
+package io.circe.yaml
+
+import io.circe.Json
+import org.scalatest.{FlatSpec, Matchers}
+
+class NumberTests extends FlatSpec with Matchers {
+  val printer = Printer.spaces2.copy(preserveOrder = true)
+  "Printer" should "print integers as expected" in {
+    import Json._
+    val json = Json.obj(
+      "string" -> fromString("x"),
+      "integer0" -> fromInt(0),
+      "integer10" -> fromInt(10),
+      "integer10000" -> fromInt(100000)
+    )
+    val output = printer.pretty(json)
+    assert(!(output contains "!!"))
+  }
+
+  it should "print doubles as expected" in {
+    import Json._
+    val json = Json.obj(
+      "string" -> fromString("x"),
+      "double0" -> fromDoubleOrNull(0),
+      "double10" -> fromDoubleOrNull(10),
+      "double10000" -> fromDoubleOrNull(100000)
+    )
+    val output = printer.pretty(json)
+    assert(!(output contains "!!"))
+  }
+}
